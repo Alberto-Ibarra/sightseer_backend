0c8507d9a9a3038c10689d04a9bd4d679f683290
Alberto-Ibarra <alberto.e.ibarra@gmail.com>
2023-03-10 15:42:22 -0800

    git ignore added

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const Sight = require('./models/sights.js');
 app.use(express.json());
 app.use(cors());
 
+const albertLogin = "admin:admin"
 
 
 
-
-const mongoURI = 'mongodb+srv://admin:admin@cluster0.nrs7e1s.mongodb.net/sightseer?retryWrites=true&w=majority';
+const mongoURI = 'mongodb+srv://'+albertLogin+'@cluster0.nrs7e1s.mongodb.net/sightseer?retryWrites=true&w=majority';
 const db = mongoose.connection;
 mongoose
 	.connect(mongoURI)
