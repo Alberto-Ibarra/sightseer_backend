de9888e205e00345d8ce07a11d68b1357a0c2147
ialoysious <isaacaloysious@gmail.com>
2023-03-12 12:17:55 -0700

    get continent

--- server.js
+++ server.js
@@ -9,17 +9,55 @@ require('dotenv').config();
 app.use(express.json());
 app.use(cors());
 
-app.post('/sights', async(req,res)=>{
-    const addSight = await Sight.create(req.body)
-    res.json(addSight);
+app.post('/sights', async (req, res) => {
+	const addSight = await Sight.create(req.body);
+	res.json(addSight);
 });
 
-app.get('/sights', async(req,res)=>{
-    const allSights= await Sight.find({});
-    res.json(allSights);
-})
+app.get('/sights', async (req, res) => {
+	const allSights = await Sight.find({});
+	res.json(allSights);
+});
+
+app.delete('/sights/:id', async (req, res) => {
+	const deleteSights = await Sight.findByIdAndDelete(req.params.id);
+	res.json(deleteSights);
+});
+
+app.put('/sights/:id', async (req, res) => {
+	const updatedSights = await Sight.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+	});
+	res.json(updatedSights);
+});
 
 
+// Worst Case.  need to make dry.
+
+app.get('/sights/asia/', async (req, res) => {
+	const continent = await Sight.find({ continent: 'Asia' });
+	res.json(continent);
+});
+app.get('/sights/africa/', async (req, res) => {
+	const continent = await Sight.find({ continent: 'Africa' });
+	res.json(continent);
+});
+app.get('/sights/northamerica/', async (req, res) => {
+	const continent = await Sight.find({ continent: 'North America' });
+	res.json(continent);
+});
+app.get('/sights/southamerica/', async (req, res) => {
+	const continent = await Sight.find({ continent: 'South America' });
+	res.json(continent);
+});
+app.get('/sights/europe/', async (req, res) => {
+	const continent = await Sight.find({ continent: 'Europe' });
+	res.json(continent);
+});
+app.get('/sights/australia/', async (req, res) => {
+	const continent = await Sight.find({ continent: 'Australia' });
+	res.json(continent);
+});
 
 const mongoURI = process.env.MONGODB;
 const db = mongoose.connection;
@@ -32,7 +70,6 @@ mongoose
 		console.log('failed to connect to mongo:', err);
 	});
 
-
-app.listen(3000, ()=>{
-    console.log("listening on port 3000");
+app.listen(3000, () => {
+	console.log('listening on port 3000');
 });
